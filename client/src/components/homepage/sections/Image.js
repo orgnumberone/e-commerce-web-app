fa93d69a78e3279b7f7a1071736ee0dbd568a79d
william <williamjzh75@gmail.com>
2022-11-19 01:19:36 +1300

    changed alt attribute in Image.js

--- client/src/components/homepage/sections/Image.js
+++ client/src/components/homepage/sections/Image.js
@@ -4,7 +4,7 @@ import pet from "../../../assets/images/background.png";
 export const Image = () => {
     return (
         <>  
-          <img src={pet} style={{width:"100%"}} alt="main-image"/>
+          <img src={pet} style={{width:"100%"}} alt="main-banner"/>
         </>
     )
 }
